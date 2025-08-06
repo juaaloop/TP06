2b99e1292582cafc676c216e62e3823296ab9130
Iara <iszwarstein@gmail.com>
2025-08-06 09:00:49 -0300

    Create Lista.cs

---
+++ Models/Lista.cs
@@ -0,0 +1,12 @@
+public class Lista{
+
+    public int idLista {get;set;}
+    public string nombre{get;set;}
+    public int idUsu {get;set;}
+
+    public crearLista(int id, string Nombre, int IdUsu){
+        idLista=id;
+        nombre=Nombre;
+        idUsu=IdUsu
+    }
+}
