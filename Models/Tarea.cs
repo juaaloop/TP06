8c9a7724ee1b150e5c830894bccbcee46b5894ff
Iara <iszwarstein@gmail.com>
2025-08-06 09:00:16 -0300

    Update Tarea.cs

--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -4,9 +4,19 @@ public class Tarea
     public int idTarea;
     public int id_usuario;
     public string nombre;
-    public DateOnly fechaCreacion;
     public bool estaFinalizada;
     public bool estaBorrada;
     public string contenido;
 
+
+    public crearTarea(string Id_usuario, string Nombre,bool final, bool borrado, string contenidos){
+        id_usuario=Id_usuario;
+        nombre=Nombre;
+        estaFinalizada=final;
+        estaBorrada=borrado;
+        contenido=contenidos;
+    }
+    public void agregarId(int id){
+        idTarea=id;
+    }
 }
