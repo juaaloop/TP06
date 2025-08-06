3b4c9b9679bc12d71a0d09999210ddff1f1e5ae8
Iara <iszwarstein@gmail.com>
2025-08-06 09:01:38 -0300

    Create BDLista.cs

---
+++ Models/BDLista.cs
@@ -0,0 +1,17 @@
+using Microsoft.Data.SqlClient;
+using Dapper;
+public static class BDLista
+{
+
+    public static string connectionString = @"Server=localhost\SQLEXPRESS01;
+    DataBase=TP06; Integrated Security=True; TrustServerCertificate=True;";
+
+    public static void agregarLista(Lista lista)
+    {
+        string query = "INSERT INTO Lista (id_lista, nombre, id_usuarioOg) VALUES (@pidLista, @pnombre,@pusuario)";
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Execute(query, new { pidLista=lista.idLista,pnombre = lista.nombre, pusuario=lista.idUsu});
+        }
+    }
+}
